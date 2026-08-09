7a937b461c1b269497ea982013ad4def09c5176d
Axit Tiwari <axittiwari0@gmail.com>
2026-08-10 00:05:13 +0530

    starting js

---
+++ Day016/L40/index.js
@@ -0,0 +1,27 @@
+// console.log("Namaste Dunia");
+
+// var age = 20;
+
+// if (true) {
+//     console.log(age);
+// }
+
+function run() {
+    var age = 25;
+    console.log(age);
+}
+
+// console.log(age); error
+run();
+
+let her = 10;
+console.log(her);
+
+her = "sam";
+console.log(her);
+
+const x = 28;
+console.log(x);
+
+// not possible
+// x = "hello";
