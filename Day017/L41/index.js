459b8cc690db38a2018ddf84f1402b8ef675f484
Axit Tiwari <axittiwari0@gmail.com>
2026-08-10 22:26:16 +0530

    js: operators, conditions, loops and strings

---
+++ Day017/L41/index.js
@@ -0,0 +1,25 @@
+
+// Arithamatic Operator
+
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+// assignment operator
+let her = "samriddhi";
+console.log(her);
+
+console.log("5" == 5);
+console.log("5" === 5);
+
+let do_i_love_her = "no";
+
+let does_she_loves_me = do_i_love_her === "yes" ? "yes" : "ofcourse";
+console.log(does_she_loves_me);
---
+++ Day017/L42/index.js
@@ -0,0 +1,5 @@
+
+// loops
+for (let i = 1; i < 10; i++) {
+    console.log("sam\n");
+}
