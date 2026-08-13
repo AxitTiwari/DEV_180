2494cd1b88019916b04ee9326be8466eb032d8b6
Axit Tiwari <axittiwari0@gmail.com>
2026-08-14 00:34:06 +0530

    just adding lecture 51 and never forget this day, [dear me] : sam

---
+++ Day020/L51/index.js
@@ -0,0 +1,9 @@
+let my_div = document.querySelector('#my-div');
+
+let new_element = document.createE
+
+new_element = document.createElement('span');
+
+new_element.textContent = "This is Me love babbar";
+
+my_div.insertAdjacentElement('beforebegin', new_element);
